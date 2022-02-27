05677cc2b52e5f778c300d1b96f09f2a2d762278
matheus <matheus.mmn@hotmail.com>
2022-02-27 09:13:07 -0300

    iniciando

---
+++ js/infantil.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    fetch("../dados/infantil.json")
+    .then(response => {
+       return response.json();
+    })
+    .then(function(myBlob) {
+        var objectURL = myBlob;
+
+        var tabInfantil = document.getElementById('infantil')
+        tabInfantil.innerHTML = ''
+        
+        for(var i = 0; i < objectURL.length; i++){
+            //console.log(objectURL[i])
+
+            Object.keys(objectURL[i]).forEach(function(item){
+                // console.log(item + " X " + objectURL[i][item]);
+                // console.log(objectURL[i][item][1]);
+                var nomes = item
+                var preco = objectURL[i][item][0]
+                var status = objectURL[i][item][1]
+
+
+                tabInfantil.innerHTML += '<tr>'+
+                                    '<td>' + nomes + '</td>'+
+                                    '<td>' + 'R$ ' + preco + '</td>'+
+                                    '<td>' + status + '</td>'+
+                                '</tr>'
+                
+               });
+
+        }
+    })
+})
---
+++ main.py
@@ -0,0 +1,174 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from json import dump
+
+nomes_revistas = []
+precos_revistas = []
+dados = []
+status = []
+
+
+class Livia:
+    def __init__(self):
+        self.url = None
+
+    @staticmethod
+    def registrar(caminho):
+        with open(f'dados/{caminho}.json', 'w') as json_file:
+            dump(dados, json_file, indent=4, ensure_ascii=False)
+    
+    def infantil(self, url):
+        self.url = url
+        url = url
+
+        response = urlopen(url)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+
+        cards_produtos = soup.findAll('tr', {'class': 'GrupoxProduto'})
+        cards_precos = soup.findAll('input', {'name': 'valor[]'})
+
+        for produto in cards_produtos:
+            nomes_revistas.append(produto.find('label').getText())
+
+        for preco in cards_precos:
+            precos_revistas.append(preco.get('value'))
+
+        for it in cards_produtos:
+            statu = it.find('b')
+            if statu is None:
+                statu = 'Disponível'
+            status.append(statu)
+
+        for n, p, s in zip(nomes_revistas, precos_revistas, status):
+            s1 = str(s).replace('<b>', '')
+            s2 = s1.replace('</b>', '')
+            dd = {}
+            dd[n] = p, s2
+            dados.append(dd)
+
+        Livia.registrar('infantil')
+        
+        print("Arquivo Criado!")
+        
+        nomes_revistas.clear()
+        precos_revistas.clear()
+        dados.clear()
+        status.clear()
+        
+    def adolescente(self, url):
+        self.url = url
+        url = url
+
+        response = urlopen(url)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+
+        cards_produtos = soup.findAll('tr', {'class': 'GrupoxProduto'})
+        cards_precos = soup.findAll('input', {'name': 'valor[]'})
+
+        for produto in cards_produtos:
+            nomes_revistas.append(produto.find('label').getText())
+
+        for preco in cards_precos:
+            precos_revistas.append(preco.get('value'))
+
+        for it in cards_produtos:
+            statu = it.find('b')
+            if statu is None:
+                statu = 'Disponível'
+            status.append(statu)
+
+        for n, p, s in zip(nomes_revistas, precos_revistas, status):
+            s1 = str(s).replace('<b>', '')
+            s2 = s1.replace('</b>', '')
+            dd = {}
+            dd[n] = p, s2
+            dados.append(dd)
+
+        Livia.registrar('adolescentes')
+        
+        print("Arquivo Criado!")
+        
+        nomes_revistas.clear()
+        precos_revistas.clear()
+        dados.clear()
+        status.clear()
+    
+    def jovens(self, url):
+        self.url = url
+        url = url
+
+        response = urlopen(url)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+
+        cards_produtos = soup.findAll('tr', {'class': 'GrupoxProduto'})
+        cards_precos = soup.findAll('input', {'name': 'valor[]'})
+
+        for produto in cards_produtos:
+            nomes_revistas.append(produto.find('label').getText())
+
+        for preco in cards_precos:
+            precos_revistas.append(preco.get('value'))
+
+        for it in cards_produtos:
+            statu = it.find('b')
+            if statu is None:
+                statu = 'Disponível'
+            status.append(statu)
+
+        for n, p, s in zip(nomes_revistas, precos_revistas, status):
+            s1 = str(s).replace('<b>', '')
+            s2 = s1.replace('</b>', '')
+            dd = {}
+            dd[n] = p, s2
+            dados.append(dd)
+
+        Livia.registrar('jovens')
+        
+        print("Arquivo Criado!")
+        
+        nomes_revistas.clear()
+        precos_revistas.clear()
+        dados.clear()
+        status.clear()
+    
+    def adultos(self, url):
+        self.url = url
+        url = url
+
+        response = urlopen(url)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+
+        cards_produtos = soup.findAll('tr', {'class': 'GrupoxProduto'})
+        cards_precos = soup.findAll('input', {'name': 'valor[]'})
+
+        for produto in cards_produtos:
+            nomes_revistas.append(produto.find('label').getText())
+
+        for preco in cards_precos:
+            precos_revistas.append(preco.get('value'))
+
+        for it in cards_produtos:
+            statu = it.find('b')
+            if statu is None:
+                statu = 'Disponível'
+            status.append(statu)
+
+        for n, p, s in zip(nomes_revistas, precos_revistas, status):
+            s1 = str(s).replace('<b>', '')
+            s2 = s1.replace('</b>', '')
+            dd = {}
+            dd[n] = p, s2
+            dados.append(dd)
+
+        Livia.registrar('adultos')
+        
+        print("Arquivo Criado!")
+        
+        nomes_revistas.clear()
+        precos_revistas.clear()
+        dados.clear()
+        status.clear()
